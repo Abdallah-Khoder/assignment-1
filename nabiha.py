facafd38171cd07edb9014a477a2a6f785c757d4
Abdallah-Khoder <abdallahkodor746@gmail.com>
2025-02-19 09:23:16 +0200

    program to input and display monthly salary and amounts for categories (Savings, Rent, Electricity)

--- nabiha.py
+++ nabiha.py
@@ -1,10 +1,12 @@
-salaryMouth = input("Enter your salary of the month : ")
-salaryMouth = input("Enter the name of the month : ")
+salaryMonth = input("Enter your salary of the month : ")
+monthName = input("Enter the name of the month : ")
 categories = ["Savings" , "Rent" , "Electricity"]
 amounts = [] 
 for i in range(len(categories)) : 
     amount = input("Enter amount for " + categories[i] + " : ")
     amounts.append(amount)
-
+print("Your salary of the mounth =  " + salaryMonth)
 for i in range(len(amounts)) : 
     print("Your amount for " + categories[i] + " = " + amounts[i])
+
+    
