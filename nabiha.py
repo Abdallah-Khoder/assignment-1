729d6b5f5ec11d20b37f3aec3bddb7a8e9f030de
Abdallah-Khoder <abdallahkodor746@gmail.com>
2025-02-20 13:25:15 +0200

    Calculate  rest  monthly salary and year expenses for electricity and rent  , and Salary power 2

--- nabiha.py
+++ nabiha.py
@@ -1,3 +1,4 @@
+import math 
 salaryMonth = input("Enter your salary of the month : ")
 monthName = input("Enter the name of the month : ")
 categories = ["Savings" , "Rent" , "Electricity"]
@@ -5,8 +6,15 @@ amounts = []
 for i in range(len(categories)) : 
     amount = input("Enter amount for " + categories[i] + " : ")
     amounts.append(amount)
-print("Your salary of the mounth =  " + salaryMonth)
+print("Your salary of the mounth =  " + salaryMonth + "$")
 for i in range(len(amounts)) : 
-    print("Your amount for " + categories[i] + " = " + amounts[i])
+    print("Your amount for " + categories[i] + " = " + amounts[i] + "$")
 
-    
+totalAmount = int(amounts[0]) +int(amounts[1]) + int(amounts[2])
+print("The total amount for the month = " + str(totalAmount) + "$")
+restSalary = int(salaryMonth) - totalAmount
+print("Your rest Salary = " + str(restSalary) + "$")
+rentElectYear = (int(amounts[1]) + int(amounts[2]))*12 
+print("Amounts for Rent and Electricity in one year = " + str(rentElectYear) + "$")
+salaryPower2 = math.pow(int(salaryMonth) , 2) 
+print("The total salary raised of power 2 = " + str(salaryPower2) + "$")
